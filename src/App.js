6dbd2256931acb3080d435b2a53552e3216ac297
Samuel <samuelsf@hotmail.co.uk>
2022-08-09 16:05:22 +0100

    finished lab with Scott and added a drop-down list too

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Container from "./containers/Container";
+
+function App() {
+	return (
+		<>
+			<h1>
+				<u>Hacker News Stories</u>
+			</h1>
+			<Container />
+		</>
+	);
+}
+
+export default App;
